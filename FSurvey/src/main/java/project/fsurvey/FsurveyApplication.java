aa8cf9a869e65a55ccdde52e356fd6660087b4e7
furk2sahin <furkannsahin.7@gmail.com>
2021-05-18 17:50:37 +0300

    Initial commit

---
+++ FSurvey/src/main/java/project/fsurvey/FsurveyApplication.java
@@ -0,0 +1,13 @@
+package project.fsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsurveyApplication.class, args);
+    }
+
+}
